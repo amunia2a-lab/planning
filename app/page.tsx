7f6cf49c4add226b8762f59916596fe68dc1e484
amunia2a-lab <amunia2a@gmail.com>
2026-03-19 10:53:26 +0100

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <main style={{padding:40,fontFamily:"sans-serif"}}>
+      <h1>Planning OK ✅</h1>
+      <p>Le widget fonctionne</p>
+    </main>
+  );
+}
