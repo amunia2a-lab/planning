66f4ed76c2365ce5e34d8749ae451e6f4f561a51
amunia2a-lab <amunia2a@gmail.com>
2026-03-23 11:44:35 +0100

    Delete app directory

--- app/layout.tsx
+++
@@ -1,7 +0,0 @@
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="fr">
-      <body style={{ margin: 0 }}>{children}</body>
-    </html>
-  );
-}
